d733cd3b099b6ea11610d0b2dcf2229f36420d8a
Mykyta <mitina1900@gmail.com>
2024-12-18 21:03:37 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,12 +1,12 @@
 class Car:
-    def __init__(self, comfort_class: int, clean_mark: int, brand: str ):
+    def __init__(self, comfort_class: int, clean_mark: int, brand: str) -> None:
         self.comfort_class = comfort_class
         self.clean_mark = clean_mark
         self.brand = brand
 
 
 class CarWashStation:
-    def __init__(self, distance_from_city_center: float, clean_power: int, average_rating: float, count_of_ratings: int):
+    def __init__(self, distance_from_city_center: float, clean_power: int, average_rating: float, count_of_ratings: int) -> None:
         self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
         self.average_rating = average_rating
@@ -16,11 +16,10 @@ class CarWashStation:
         if self.distance_from_city_center == 0:
             return 0.0
 
-        price = (
-            car.comfort_class *
-            (self.clean_power - car.clean_mark) *
-            (self.average_rating / self.distance_from_city_center)
-        )
+        price = (car.comfort_class *
+                 (self.clean_power - car.clean_mark) *
+                 (self.average_rating / self.distance_from_city_center))
+
         return round(price, 1)
 
     def wash_single_car(self, car: Car) -> None:
@@ -36,6 +35,7 @@ class CarWashStation:
             if car.clean_mark < self.clean_power:
                 income += self.calculate_washing_price(car)
                 self.wash_single_car(car)
+
         return round(income, 1)
 
     def rate_service(self, rate: float) -> None:
